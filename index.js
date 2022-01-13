73036037fab2e3eaae860ef25b7917f00befbc9b
Lindsey <lindsey.pregent@gmail.com>
2022-01-13 11:07:19 -0500

    commit tree b5af13ca72362f3e30f76f0d0e47c2fddf18d16a

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+
+// function superbowlWin(record) {
+//     let result = record.find(record => record.year === "W")
+//     return result ? result.year : undefined;
+// }
+
+const superbowlWin = (record) => {
+    const result = record.find(record => record.result === "W");
+    // return result ? result.year : undefined;
+    if (result) {
+        return result.year 
+    } else {
+        return undefined
+    }
+}
+
+
+   
